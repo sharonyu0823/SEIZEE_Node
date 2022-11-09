c3f6be2a04193564d13d2e90127cfb9581d291bc
Sharon Yu <yu5286pp@gmail.com>
2022-11-09 21:33:44 +0800

    foundation

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// top-level middleware
+app.use(express.json());
+
+
+// routes
+// 01-cart
+
+// 02-forum
+
+// 03-shop
+
+// 04-product
+
+// 05-member
+
+// 06-event
+
+
+// 環境設定
+
+const port = process.env.SERVER_PORT || 3002;
+app.listen(port, () => {
+  console.log("server started");
+});
---
+++ modules/db_connect.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'root',
+    database: process.env.DB_NAME || 'no_waste',
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0
+});
+
+module.exports = pool.promise();
