0d012a100131694caaa7106a50a3af34c016f74f
Ruby Lee <rubylee8656@gmail.com>
2022-11-22 12:11:54 +0800

    fix cate-sql

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const db = require(__dirname + "/../modules/db_connect");
 const cors = require("cors");
 
-router.get('/',  async(req, res, next)=>{
-    const shop_sql = "SELECT s.*, c.`shop_city`, a.`shop_area` FROM `shop_list` s JOIN `shop_address_city` c ON s.`shop_address_city_sid` = c.`sid` JOIN `shop_address_area` a ON s.`shop_address_area_sid` = a.`sid`";
-    const [shop_rows] = await db.query(shop_sql);
-    res.json({ shop_rows});
-  });
+// router.get('/test',  async(req, res, next)=>{
+//     const shop_sql = "SELECT s.*, c.`shop_city`, a.`shop_area` FROM `shop_list` s JOIN `shop_address_city` c ON s.`shop_address_city_sid` = c.`sid` JOIN `shop_address_area` a ON s.`shop_address_area_sid` = a.`sid`";
+//     const [shop_rows] = await db.query(shop_sql);
+//     res.json({ shop_rows});
+//   });
+
 router.get('/shop_city',  async(req, res, next)=>{
     const city_sql = "SELECT * FROM `shop_address_city`";
     const [city_rows] = await db.query(city_sql);
@@ -23,25 +24,26 @@ router.get('/shop_cate',  async(req, res, next)=>{
     const [cate_rows] = await db.query(cate_sql);
     res.json({ cate_rows});
   });
-router.get('/test',  async(req, res, next)=>{
-    const test_sql = "SELECT s.*, c.`shop_city`, a.`shop_area`, f.`product_name`, fc.`product_categories` FROM `shop_list` s JOIN `shop_address_city` c ON s.`shop_address_city_sid` = c.`sid` JOIN `shop_address_area` a ON s.`shop_address_area_sid` = a.`sid` JOIN `food_product` f ON s.`sid` = f.`shop_list_sid` JOIN `food_category` fc ON f.`product_categories_sid` = fc.`sid`";
-    const [test_rows] = await db.query(test_sql);
+router.get('/',  async(req, res, next)=>{
+    const shop_sql = "SELECT s.*, c.`shop_city`, a.`shop_area` ,fc.`product_categories` FROM `shop_list` s JOIN `shop_address_city` c ON s.`shop_address_city_sid` = c.`sid` JOIN `shop_address_area` a ON s.`shop_address_area_sid` = a.`sid` JOIN `food_product` f ON s.`sid` = f.`shop_list_sid` JOIN `food_category` fc ON f.`product_categories_sid` = fc.`sid`";
+    const [shop_rows] = await db.query(shop_sql);
     const shop_dic = {};
-    test_rows.forEach(r=>{
-      const cate = r.product_categories
-      if(shop_dic[r.sid]){
-        if(!shop_dic[r.sid].cates.includes(cate)){
-          shop_dic[r.sid].cates.push(cate);
+    shop_rows.forEach(rows=>{
+      const cate = rows.product_categories
+      if(shop_dic[rows.sid]){
+        if(!shop_dic[rows.sid].cates.includes(cate)){
+          shop_dic[rows.sid].cates.push(cate);
         }
       } else {
-        shop_dic[r.sid] = {r, cates:[cate]};
+        shop_dic[rows.sid] = {rows, cates:[cate]};
       }
-
-
     });
-
-
-    res.json(shop_dic);
+    
+    res.json(Object.values(shop_dic));
+    //取出店家名稱
+    // res.json(shop_dic[1].rows.shop_name);
+    //取出種類名稱 第一個
+    // res.json(shop_dic[1].cates[0]);
   });
   
 module.exports = router;
