2827cffebdb0e052f55cdd0516dc42a00ab18571
Ruby Lee <rubylee8656@gmail.com>
2022-11-24 10:22:39 +0800

    demoShop done

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,23 @@ const router = express.Router();
 const db = require(__dirname + "/../modules/db_connect");
 const cors = require("cors");
 
-// router.get('/test',  async(req, res, next)=>{
-//     const shop_sql = "SELECT s.*, c.`shop_city`, a.`shop_area` FROM `shop_list` s JOIN `shop_address_city` c ON s.`shop_address_city_sid` = c.`sid` JOIN `shop_address_area` a ON s.`shop_address_area_sid` = a.`sid`";
-//     const [shop_rows] = await db.query(shop_sql);
-//     res.json({ shop_rows});
-//   });
+router.get('/shop_demo',  async(req, res, next)=>{
+    const demo_sql = "SELECT s.* , c.`shop_city`, a.`shop_area` ,fc.`product_categories` FROM `shop_list` s JOIN `shop_address_city` c ON s.`shop_address_city_sid` = c.`sid` JOIN `shop_address_area` a ON s.`shop_address_area_sid` = a.`sid` JOIN `food_product` f ON s.`sid` = f.`shop_list_sid` JOIN `food_category` fc ON f.`product_categories_sid` = fc.`sid` WHERE `shop_address_city_sid` = 2 AND `shop_address_area_sid`= 8";
+    const [demo_rows] = await db.query(demo_sql);
+    const demo_dic = {};
+    demo_rows.forEach(rows=>{
+      const cate = rows.product_categories
+      if(demo_dic[rows.sid]){
+        if(!demo_dic[rows.sid].cates.includes(cate)){
+          demo_dic[rows.sid].cates.push(cate);
+        }
+      } else {
+        demo_dic[rows.sid] = {rows, cates:[cate]};
+      }
+    });
+    
+    res.json(Object.values(demo_dic));
+  });
 
 router.get('/shop_city',  async(req, res, next)=>{
     const city_sql = "SELECT * FROM `shop_address_city`";
