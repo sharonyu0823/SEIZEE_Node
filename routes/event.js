cc7bb98e841dd397c509a14b548ab3d0e8a7fdde
James Tsai <acidman1127@gmail.com>
2022-11-26 14:02:38 +0800

    11/26-14:02

--- routes/event.js
+++ routes/event.js
@@ -3,33 +3,38 @@ const router = express.Router();
 const db = require(__dirname + "/../modules/db_connect");
 const cors = require("cors"); 
 
-router.get('/event-test/workshop', async(req, res, next)=>{
-    const test_sql = "SELECT * FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid` WHERE `cate` = 1;";
+router.get('/event-test', async(req, res, next)=>{
+    const test_sql = "SELECT event_all.*, event_style.styles FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid`;";
     const [test_rows] = await db.query(test_sql);
-    res.json({test_rows});
+    res.json(test_rows);
 })
-router.get('/event-test/music', async(req, res, next)=>{
-    const test_sql = "SELECT * FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid` WHERE `cate` = 2;";
-    const reigistered = "SELECT COUNT (1) FROM `event_registered` WHERE `event_all`.`event_sid` = 5";
-    const [test_rows] = await db.query(test_sql);
-    // const [reigistered_rows] = await db.query(reigistered);
-    // console.log({registered_rows});
-    res.json({test_rows});
-})
-router.get('/event-test/seminar', async(req, res, next)=>{
-    const test_sql = "SELECT * FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid` WHERE `cate` = 3;";
-    const [test_rows] = await db.query(test_sql);
-    res.json({test_rows});
-})
-router.get('/event-test/vr', async(req, res, next)=>{
-    const test_sql = "SELECT * FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid` WHERE `cate` = 4;";
-    const [test_rows] = await db.query(test_sql);
-    res.json({test_rows});
-})
-router.get('/event-test/theater', async(req, res, next)=>{
-    const test_sql = "SELECT * FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid` WHERE `cate` = 5;";
-    const [test_rows] = await db.query(test_sql);
-    res.json({test_rows});
-})
+// router.get('/event-test/workshop', async(req, res, next)=>{
+//     const test_sql = "SELECT * FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid` WHERE `cate` = 1;";
+//     const [test_rows] = await db.query(test_sql);
+//     res.json({test_rows});
+// })
+// router.get('/event-test/music', async(req, res, next)=>{
+//     const test_sql = "SELECT * FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid` WHERE `cate` = 2;";
+//     const reigistered = "SELECT COUNT (1) FROM `event_registered` WHERE `event_all`.`event_sid` = 5";
+//     const [test_rows] = await db.query(test_sql);
+//     // const [reigistered_rows] = await db.query(reigistered);
+//     // console.log({registered_rows});
+//     res.json({test_rows});
+// })
+// router.get('/event-test/seminar', async(req, res, next)=>{
+//     const test_sql = "SELECT * FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid` WHERE `cate` = 3;";
+//     const [test_rows] = await db.query(test_sql);
+//     res.json({test_rows});
+// })
+// router.get('/event-test/vr', async(req, res, next)=>{
+//     const test_sql = "SELECT * FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid` WHERE `cate` = 4;";
+//     const [test_rows] = await db.query(test_sql);
+//     res.json({test_rows});
+// })
+// router.get('/event-test/theater', async(req, res, next)=>{
+//     const test_sql = "SELECT * FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid` WHERE `cate` = 5;";
+//     const [test_rows] = await db.query(test_sql);
+//     res.json({test_rows});
+// })
 
 module.exports = router;
