8992dbb5bd9086e864ac0d3b790c855507c016e1
Ruby Lee <rubylee8656@gmail.com>
2022-11-28 01:27:11 +0800

    change shopHome api

--- routes/shop.js
+++ routes/shop.js
@@ -4,11 +4,11 @@ const db = require(__dirname + "/../modules/db_connect");
 const cors = require("cors");
 
 router.get('/shop_demo',  async(req, res, next)=>{
-    const demo_sql = "SELECT s.* , c.`shop_city`, a.`shop_area` ,fc.`product_categories` FROM `shop_list` s JOIN `shop_address_city` c ON s.`shop_address_city_sid` = c.`sid` JOIN `shop_address_area` a ON s.`shop_address_area_sid` = a.`sid` JOIN `food_product` f ON s.`sid` = f.`shop_list_sid` JOIN `food_category` fc ON f.`product_categories_sid` = fc.`sid` WHERE `shop_address_city_sid` = 2 AND `shop_address_area_sid`= 8";
+    const demo_sql = "SELECT s.* , c.`shop_city`, a.`shop_area` ,fc.`category_name` FROM `shop_list` s JOIN `shop_address_city` c ON s.`shop_address_city_sid` = c.`sid` JOIN `shop_address_area` a ON s.`shop_address_area_sid` = a.`sid` JOIN `food_product` f ON s.`sid` = f.`shop_list_sid` JOIN `product_category` fc ON f.`product_category_sid` = fc.`sid` WHERE `shop_address_city_sid` = 2 AND `shop_address_area_sid`= 8";
     const [demo_rows] = await db.query(demo_sql);
     const demo_dic = {};
     demo_rows.forEach(rows=>{
-      const cate = rows.product_categories
+      const cate = rows.category_name
       if(demo_dic[rows.sid]){
         if(!demo_dic[rows.sid].cates.includes(cate)){
           demo_dic[rows.sid].cates.push(cate);
@@ -32,16 +32,16 @@ router.get('/shop_area',  async(req, res, next)=>{
     res.json({ area_rows});
   });
 router.get('/shop_cate',  async(req, res, next)=>{
-    const cate_sql = "SELECT * FROM `food_category`";
+    const cate_sql = "SELECT * FROM `product_category`";
     const [cate_rows] = await db.query(cate_sql);
     res.json({ cate_rows});
   });
 router.get('/',  async(req, res, next)=>{
-    const shop_sql = "SELECT s.*, c.`shop_city`, a.`shop_area` ,fc.`product_categories` FROM `shop_list` s JOIN `shop_address_city` c ON s.`shop_address_city_sid` = c.`sid` JOIN `shop_address_area` a ON s.`shop_address_area_sid` = a.`sid` JOIN `food_product` f ON s.`sid` = f.`shop_list_sid` JOIN `food_category` fc ON f.`product_categories_sid` = fc.`sid`";
+    const shop_sql = "SELECT s.*, c.`shop_city`, a.`shop_area` ,fc.`category_name` FROM `shop_list` s JOIN `shop_address_city` c ON s.`shop_address_city_sid` = c.`sid` JOIN `shop_address_area` a ON s.`shop_address_area_sid` = a.`sid` JOIN `food_product` f ON s.`sid` = f.`shop_list_sid` JOIN `product_category` fc ON f.`product_category_sid` = fc.`sid`";
     const [shop_rows] = await db.query(shop_sql);
     const shop_dic = {};
     shop_rows.forEach(rows=>{
-      const cate = rows.product_categories
+      const cate = rows.category_name
       if(shop_dic[rows.sid]){
         if(!shop_dic[rows.sid].cates.includes(cate)){
           shop_dic[rows.sid].cates.push(cate);
