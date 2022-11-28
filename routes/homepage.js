f0f37920cb89d4977c0411835b63bf4d28b152a0
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-11-28 23:14:05 +0800

    add router for news-crawl

--- routes/homepage.js
+++ routes/homepage.js
@@ -4,9 +4,27 @@ const db = require(__dirname + '/../modules/db_connect');
 const cors = require("cors");
 //const { application } = require("express");
 
+router.get('/news-crawl', async(req, res) => {
+    const recipeNews = "SELECT * FROM `cooking_post` JOIN `member` ON `cooking_post`.`member_sid` = `member`.`mb_sid` ORDER BY `sid` DESC LIMIT 1";
+    const [recipeNewsRow] = await db.query(recipeNews);
+    
+    const eventNews = "SELECT * FROM `event_all` ORDER BY `sid` DESC LIMIT 1";
+    const [eventNewsRow] = await db.query(eventNews);
+
+    const seizeeNews = "SELECT * FROM `official_post` ORDER BY `sid` LIMIT 1";
+    const [seizeeNewsRow] = await db.query(seizeeNews);
+
+    const newsCrawlRows = {
+        'recipeNewsRow': recipeNewsRow[0],
+        'eventNewsRow': eventNewsRow[0],
+        'seizeeNewsRow': seizeeNewsRow[0],
+    };
+
+    res.json(newsCrawlRows);
+})
 
 router.get('/recipe-posts', async(req, res) => {
-    const recipePost = "SELECT * FROM `cooking_post` JOIN `member` ON `cooking_post`.`member_sid` = `member`.`mb_sid` ORDER BY `sid` DESC";
+    const recipePost = "SELECT * FROM `cooking_post` JOIN `member` ON `cooking_post`.`member_sid` = `member`.`mb_sid` ORDER BY `sid`";
     const [recipePostRows] = await db.query(recipePost);
     
     res.json({recipePostRows});
