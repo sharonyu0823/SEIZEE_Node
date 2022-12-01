4b0e65a08734d5c6aa55dc4ce9d83ef35646d3df
Ruby Lee <rubylee8656@gmail.com>
2022-12-01 08:47:30 +0800

    change db connect to miee

--- modules/db_connect.js
+++ modules/db_connect.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || 'root',
-    database: process.env.DB_NAME || '04-no-waste',
+    database: process.env.DB_NAME || 'seizee',
     waitForConnections: true,
     connectionLimit: 5,
     queueLimit: 0
