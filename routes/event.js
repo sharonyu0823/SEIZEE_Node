193d62eef9ae7ce73f02ec832687ade6f01a51ff
James Tsai <acidman1127@gmail.com>
2022-12-07 16:20:06 +0800

    1619

--- routes/event.js
+++ routes/event.js
@@ -3,21 +3,52 @@ const router = express.Router();
 const db = require(__dirname + "/../modules/db_connect");
 const cors = require("cors"); 
 
-router.get('/event-test', async(req, res)=>{
+router.post('/event-test', async(req, res)=>{
+    const {memberSid} = req.body
     const test_sql = "SELECT event_all.*, event_style.styles FROM `event_all` JOIN `event_style` ON `event_all`.`style` = `event_style`.`sid`;";
     const [test_rows] = await db.query(test_sql);
+    // console.log(test_rows)
+    for (let row of test_rows) {
+        const sql = "SELECT * FROM event_likes WHERE member_sid = ? AND event_sid = ?"
+        const [rows] = await db.query(sql, [memberSid, row.sid]);
+        if (rows.length) {
+            row.like = true;
+        } else {
+            row.like = false;
+        }
+    }
+
     res.json(test_rows); 
+})
+
+router.post('/event-add', async(req, res)=>{
+    const {memberSid, eventSid} = req.body
+    const sql_likes = "INSERT INTO event_likes (member_sid, event_sid, created_at) VALUES (?, ?, current_timestamp());";
+    const [test_rows] = await db.query(sql_likes, [memberSid, eventSid]);
+    res.json({
+        ok: true
+    }); 
 })
 router.post('/event-ticket', async(req, res)=>{
     const {memberSid, timeTable} = req.body
     if(timeTable && timeTable.length) {
         for(let item of timeTable){
             if(item.name){
+
+                // const event_sql = "SELECT * FROM `event_registered` WHERE member_sid = ?"
+                // const [event_rows] = await db.query(event_sql, [
+                //     memberSid,
+                //     item.sid,
+                // ]);
+
+
                 const test_sql = "INSERT INTO event_registered (member_sid, event_sid) VALUES (?, ?)";
                 const [test_rows] = await db.query(test_sql, [
                     memberSid,
                     item.sid,
                 ]);
+                const sql = "UPDATE event_all SET registered = registered + 1 WHERE sid = ? ";
+                const [rows] = await db.query(sql, [item.sid])
                 console.log('tset12312331')
             }
     
